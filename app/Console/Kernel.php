62cc4ba51eb6716aba3cbfab4a32ffc268ddb794
Ruslan <abdullin.dev@gmail.com>
2023-02-13 00:07:10 +0500

    edit readme.me

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -15,7 +15,9 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')->hourly();
+        $schedule->command('transfer:send')
+            ->dailyAt('06:00')
+            ->withoutOverlapping();
     }
 
     /**
